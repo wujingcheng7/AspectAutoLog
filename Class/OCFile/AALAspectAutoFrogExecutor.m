506792433d9d7b98ddf9ef7117a1524477a8e401
wujingcheng7 <daniel.wu@yyinedu.com>
2024-03-26 11:39:26 +0800

    add AALAspectAutoFrogExecutor file

---
+++ Class/OCFile/AALAspectAutoFrogExecutor.m
@@ -0,0 +1,10 @@
+//
+//  AALAspectAutoFrogExecutor.m
+//  AspectAutoLog
+//
+//  Created by 吴京城 on 2024/3/26.
+//
+
+#import "AALAspectAutoFrogExecutor.h"
+
+Class<AALAspectAutoLogProtocol> AALAspectAutoFrogExecutor;
--- Class/OCFile/AALAspectAutoLogProtocol.h
+++ Class/OCFile/AALAspectAutoLogProtocol.h
@@ -27,31 +27,3 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 NS_ASSUME_NONNULL_END
-
-/// 需要外界对这个类进行初始化，建议在 NSObject 的 "+load" 方法内执行初始化
-/// External initialization of this class is required. It is recommended to do this within the NSObject's "+load" method.
-///
-/// Example:
-///
-/// ----- YourCustomLog.h file -----
-/// #import <Foundation/Foundation.h>
-///
-/// @interface NSObject (YourCustomLog)
-///
-/// @end
-///
-///
-/// ----- YourCustomLog.m file -----
-/// #import "NSObject+YourCustomLog.h"
-/// #import <AspectAutoLog/AALAspectAutoLogProtocol.h>
-///
-/// @implementation NSObject (YourCustomLog)
-///
-/// + (void)load {
-///     // YourCustomLogClass must implement AALAspectAutoLogProtocol
-///     AALAspectAutoFrogExecutor = [YourCustomLogClass class];
-/// }
-///
-/// @end
-///
-extern _Nullable Class<AALAspectAutoLogProtocol> AALAspectAutoFrogExecutor;
